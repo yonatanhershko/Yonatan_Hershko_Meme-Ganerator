c8bb26c6b053ac42ce437bed4b4dd90906c164f9
Yonatan Hershko <yonher8@gmail.com>
2024-06-13 19:57:25 +0300

    Add footer header and edit meme first style

--- js/controllers/main-controller.js
+++ js/controllers/main-controller.js
@@ -6,13 +6,13 @@ function onInit() {
     renderMeme()
     renderGallery()
     document.querySelector('.gallery-container').classList.remove('hidden')
-    document.querySelector('.canvas-container').classList.add('hidden')
+    document.querySelector('.editor-container').classList.add('hidden')
 }
 
 
 function switchView() {
     const galleryContainer = document.querySelector('.gallery-container')
-    const canvasContainer = document.querySelector('.canvas-container')
+    const canvasContainer = document.querySelector('.editor-container')
     galleryContainer.classList.toggle('hidden')
     canvasContainer.classList.toggle('hidden')
 }
